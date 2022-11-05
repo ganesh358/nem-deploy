fc0f5ff49cc9f0708f9cd343727dfd14de05594e
ganesh358 <ganeshmoremc@gmail.com>
2022-11-05 18:59:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+
+
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000
+
+
+
+app.listen(PORT , () => {
+    console.log(`Listening on PORT ${PORT}` )
+})
